bab27ee9127d26d6b6cb1d717ad3059f4b1872ab
Andrew Nadig <a.w.nadig@gmail.com>
2019-10-16 19:22:33 -0500

    recent documents

--- app/main.js
+++ app/main.js
@@ -515,6 +515,7 @@ function openTournament(focusedWindow) {
           currentFile = fileNameAry[0]; //open dialog doesn't allow selecting multiple files
           focusedWindow.webContents.send('openTournament', currentFile);
           unsavedData = false;
+          app.addRecentDocument(currentFile);
         }
       }
     );
