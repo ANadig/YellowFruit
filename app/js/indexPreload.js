195b75448cfa292787f3e1d6fdb1eee7bb3a5091
alopezlago <alopezlago@cs.hmc.edu>
2022-01-10 23:22:33 -0800

    Fix issue with on not being exposed

--- app/js/indexPreload.js
+++ app/js/indexPreload.js
@@ -9,19 +9,35 @@ const ipcRenderer = electron.ipcRenderer;
 const appDataPath = ipcRenderer.sendSync('getAppDataPath');
 console.log(appDataPath);
 const userConfigFolderProd = Path.resolve(appDataPath, 'YellowFruit');
+const appFolder = Path.resolve(__dirname, '..');
 
-contextBridge.exposeInMainWorld('RELEASED_RPT_CONFIG_FILE', Path.resolve(__dirname, '..', 'data', 'ReleasedRptConfig.json'));
+contextBridge.exposeInMainWorld('RELEASED_RPT_CONFIG_FILE', Path.resolve(appFolder, '..', 'data', 'ReleasedRptConfig.json'));
 contextBridge.exposeInMainWorld('USER_CONFIG_FOLDER_PROD', userConfigFolderProd);
 contextBridge.exposeInMainWorld('CUSTOM_RPT_CONFIG_FILE_PROD', Path.resolve(userConfigFolderProd, 'CustomRptConfig.json'));
-contextBridge.exposeInMainWorld('CUSTOM_RPT_CONFIG_FILE_DEV', Path.resolve(__dirname, '..', 'data', 'CustomRptConfig.json'));
-contextBridge.exposeInMainWorld('defaultStandingsLocation', Path.resolve(__dirname, 'standings.html'));
-contextBridge.exposeInMainWorld('defaultIndividualsLocation', Path.resolve(__dirname, 'individuals.html'));
-contextBridge.exposeInMainWorld('defaultScoreboardLocation', Path.resolve(__dirname, 'games.html'));
-contextBridge.exposeInMainWorld('defaultTeamDetailLocation', Path.resolve(__dirname, 'teamdetail.html'));
-contextBridge.exposeInMainWorld('defaultPlayerDetailLocation', Path.resolve(__dirname, 'playerdetail.html'));
-contextBridge.exposeInMainWorld('defaultRoundReportLocation', Path.resolve(__dirname, 'rounds.html'));
+contextBridge.exposeInMainWorld('CUSTOM_RPT_CONFIG_FILE_DEV', Path.resolve(appFolder, '..', '..', 'data', 'CustomRptConfig.json'));
+contextBridge.exposeInMainWorld('defaultStandingsLocation', Path.resolve(appFolder, 'standings.html'));
+contextBridge.exposeInMainWorld('defaultIndividualsLocation', Path.resolve(appFolder, 'individuals.html'));
+contextBridge.exposeInMainWorld('defaultScoreboardLocation', Path.resolve(appFolder, 'games.html'));
+contextBridge.exposeInMainWorld('defaultTeamDetailLocation', Path.resolve(appFolder, 'teamdetail.html'));
+contextBridge.exposeInMainWorld('defaultPlayerDetailLocation', Path.resolve(appFolder, 'playerdetail.html'));
+contextBridge.exposeInMainWorld('defaultRoundReportLocation', Path.resolve(appFolder, 'rounds.html'));
 
-contextBridge.exposeInMainWorld('ipcRenderer', electron.ipcRenderer);
-// contextBridge.exposeInMainWorld('electron', electron);
+// This is unsafe, we should expose a type-safe set of messages for communication. See 
+// https://www.electronjs.org/docs/latest/tutorial/context-isolation/
+// This uses a trick from https://github.com/electron/electron/issues/21437#issuecomment-880929111
+contextBridge.exposeInMainWorld('ipcRenderer', {
+    ...electron.ipcRenderer,
+    on: (channel, listener) => {
+        ipcRenderer.on(channel, listener);
+        return () => {
+            ipcRenderer.removeListener(channel, listener);
+        };
+    }
+});
+
+electron.ipcRenderer.on
+
+// TODO: We should lock this down so we don't allow arbitrary read/writes
+contextBridge.exposeInMainWorld('fs', { writeFileSync: fs.writeFileSync, readFileSync: fs.readFileSync, existsSync: fs.existsSync });
 
 console.log('End of preload!');
