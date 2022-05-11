b9b57fef6905e4bda5bee6c895ede9855d1e6181
Andrew Nadig <a.w.nadig@gmail.com>
2022-05-10 20:37:09 -0500

    #34

--- process/ts/TeamListEntry.tsx
+++ process/ts/TeamListEntry.tsx
@@ -94,7 +94,7 @@ export class TeamListEntry extends React.Component<TeamListEntryProps, TeamListE
       );
     }
     const gameWord = this.props.numGamesPlayed == 1 ? 'game' : 'games';
-    const tooltip = this.props.team.teamName + ' has played ' + this.props.numGamesPlayed + ' ' + gameWord;
+    const tooltip = 'This team has played ' + this.props.numGamesPlayed + ' ' + gameWord;
     return (
       <span className="secondary-content btn-flat disabled-item-delete tooltipped"
         data-tooltip={tooltip} data-position="left">
