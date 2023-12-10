26c51583bc0733dd8213022b39c62b35b60187b1
Andrew Nadig <a.w.nadig@gmail.com>
2023-12-10 14:16:13 -0600

    unit test setup and first few tests

---
+++ src/__tests__/TournamentManager01.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from 'vitest';
+import { TournamentManager } from '../renderer/TournamentManager';
+
+// #region setTournamentName
+// basic test
+test('setTournamentName01', () => {
+  const mgr = new TournamentManager();
+  const expected = 'abc';
+  mgr.setTournamentName(expected);
+  expect(mgr.tournament.name).toBe(expected);
+});
+
+// unsaved data flag
+test('setTournamentName02', () => {
+  const mgr = new TournamentManager();
+  expect(mgr.unsavedData).toBeFalsy();
+
+  mgr.setTournamentName('abc');
+  expect(mgr.unsavedData).toBeTruthy();
+
+  mgr.unsavedData = false;
+  mgr.setTournamentName('abc');
+  expect(mgr.unsavedData).toBeFalsy();
+});
+
+// trim whitspace
+test('setTournamentName03', () => {
+  const mgr = new TournamentManager();
+  mgr.setTournamentName(' abc ');
+  expect(mgr.tournament.name).toBe('abc');
+});
+// #endregion
+
+// #region setTournamentLocation
+// basic test + strip whitespace + unsaved data
+test('setTournamentSiteName01', () => {
+  const mgr = new TournamentManager();
+  expect(mgr.unsavedData).toBeFalsy();
+
+  mgr.setTournamentSiteName(' abc ');
+  expect(mgr.tournament.tournamentSite.name).toBe('abc');
+  expect(mgr.unsavedData).toBeTruthy();
+});
+// #endregion
