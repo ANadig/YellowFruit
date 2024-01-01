28bfdf7fbe170903663e58de2311582b3491c4fd
Andrew Nadig <a.w.nadig@gmail.com>
2024-01-01 01:09:59 -0600

    UI layout for the standard rule set card

--- src/renderer/Components/StandardRuleSetCard.tsx
+++ src/renderer/Components/StandardRuleSetCard.tsx
@@ -1,7 +1,34 @@
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { useState } from 'react';
 import YfCard from './YfCard';
+import { CommonRuleSets, ScoringRules } from '../DataModel/ScoringRules';
+
+// Defines the order the buttons should be in
+const ruleSets = [CommonRuleSets.NaqtUntimed, CommonRuleSets.NaqtTimed, CommonRuleSets.Acf, CommonRuleSets.Pace];
 
 function StandardRuleSetCard() {
-  return <YfCard title="Standard Rule Sets">hello</YfCard>;
+  const [ruleSet, setRuleSet] = useState('1');
+
+  return (
+    <YfCard title="Standard Rule Sets">
+      <ToggleButtonGroup
+        size="small"
+        color="primary"
+        exclusive
+        value={ruleSet}
+        onChange={(e, newValue) => {
+          if (newValue === null) return;
+          setRuleSet(newValue);
+        }}
+      >
+        {ruleSets.map((val) => (
+          <ToggleButton key={val} value={val}>
+            {ScoringRules.getRuleSetName(val)}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </YfCard>
+  );
 }
 
 export default StandardRuleSetCard;
