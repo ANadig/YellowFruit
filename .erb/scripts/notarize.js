fdff5f1fe65dad687fa1af8b5ec065e6d27cd3d0
Andrew Nadig <a.w.nadig@gmail.com>
2025-06-23 20:47:02 -0500

    notarization script needs teamId

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -24,5 +24,6 @@ exports.default = async function notarizeMacos(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
   });
 };
